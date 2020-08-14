8a4b450e8ae51568283663f720d6ff19479becc8
fire015 <emailfire@gmail.com>
2020-08-14 16:06:37 +0100

    Remove context menu before re-adding

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,13 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    title: "Report issue with website",
-    contexts: ["browser_action"],
-    onclick: () => {
-      chrome.tabs.create({ url: "https://github.com/fire015/adblocker-notice-removal/issues/new" });
-    },
+  chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+      title: "Report issue with website",
+      contexts: ["browser_action"],
+      onclick: () => {
+        chrome.tabs.create({
+          url: "https://github.com/fire015/adblocker-notice-removal/issues/new",
+        });
+      },
+    });
   });
 });
