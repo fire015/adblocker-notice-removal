dce7e8d1235b325a50ebc8bc666b3ab290ee1cd8
fire015 <emailfire@gmail.com>
2020-09-21 15:39:36 +0100

    Make it easier to report issues

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,15 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.removeAll(() => {
-    chrome.contextMenus.create({
-      title: "Report issue with website",
-      contexts: ["browser_action"],
-      onclick: () => {
-        chrome.tabs.create({
-          url: "https://github.com/fire015/adblocker-notice-removal/issues/new",
-        });
-      },
-    });
+  chrome.contextMenus.create({
+    id: "report",
+    title: "Report issue with website",
+    contexts: ["browser_action"],
   });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "report") {
+    chrome.tabs.create({
+      url: "https://github.com/fire015/adblocker-notice-removal/issues/new",
+    });
+  }
 });
