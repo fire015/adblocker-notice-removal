83fe53cd85c5668874726653478a49975d607010
fire015 <emailfire@gmail.com>
2021-07-13 13:25:35 +0100

    Fix badge timer

--- src/actions.js
+++ src/actions.js
@@ -2,9 +2,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "showBadge") {
     chrome.action.setBadgeBackgroundColor({ color: "#6b6b6b" });
     chrome.action.setBadgeText({ text: "✓" });
-
-    setTimeout(() => {
-      chrome.action.setBadgeText({ text: "" });
-    }, 3000);
+    chrome.alarms.create({ when: Date.now() + 3000 });
   }
+});
+
+chrome.alarms.onAlarm.addListener(() => {
+  chrome.action.setBadgeText({ text: "" });
 });
