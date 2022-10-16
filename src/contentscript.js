f308fa42311539c807fc19cd2bb03a398ba6abdc
fire015 <emailfire@gmail.com>
2022-10-16 10:08:38 +0100

    Use hostname matching instead of regex

--- src/contentscript.js
+++ src/contentscript.js
@@ -74,65 +74,8 @@ const removeElement = (el, e, attempts) => {
   checkOverflowHidden();
 };
 
-const isURLMatched = (url, urlTemplate) => {
-  const regex = createUrlRegex(urlTemplate);
-  return Boolean(url.match(regex));
-};
-
-// https://github.com/darkreader/darkreader/blob/master/src/utils/url.ts
-const createUrlRegex = (urlTemplate) => {
-  urlTemplate = urlTemplate.trim();
-  const exactBeginning = urlTemplate[0] === "^";
-  const exactEnding = urlTemplate[urlTemplate.length - 1] === "$";
-
-  urlTemplate = urlTemplate
-    .replace(/^\^/, "") // Remove ^ at start
-    .replace(/\$$/, "") // Remove $ at end
-    .replace(/^.*?\/{2,3}/, "") // Remove scheme
-    .replace(/\?.*$/, "") // Remove query
-    .replace(/\/$/, ""); // Remove last slash
-
-  let slashIndex;
-  let beforeSlash;
-  let afterSlash;
-
-  if ((slashIndex = urlTemplate.indexOf("/")) >= 0) {
-    beforeSlash = urlTemplate.substring(0, slashIndex); // google.*
-    afterSlash = urlTemplate.replace("$", "").substring(slashIndex); // /login/abc
-  } else {
-    beforeSlash = urlTemplate.replace("$", "");
-  }
-
-  // SCHEME and SUBDOMAINS
-  let result = exactBeginning
-    ? "^(.*?\\:\\/{2,3})?" // Scheme
-    : "^(.*?\\:\\/{2,3})?([^/]*?\\.)?"; // Scheme and subdomains
-
-  // HOST and PORT
-  const hostParts = beforeSlash.split(".");
-  result += "(";
-
-  for (let i = 0; i < hostParts.length; i++) {
-    if (hostParts[i] === "*") {
-      hostParts[i] = "[^\\.\\/]+?";
-    }
-  }
-
-  result += hostParts.join("\\.");
-  result += ")";
-
-  // PATH and QUERY
-  if (afterSlash) {
-    result += "(";
-    result += afterSlash.replace("/", "\\/");
-    result += ")";
-  }
-
-  result += exactEnding
-    ? "(\\/?(\\?[^/]*?)?)$" // All following queries
-    : "(\\/?.*?)$"; // All following paths and queries
-
-  return new RegExp(result, "i");
+const isHostnameMatched = (hostname, hostnameToMatch) => {
+  return hostname.slice(-hostnameToMatch.length) === hostnameToMatch;
 };
 
 const run = (rules) => {
@@ -140,7 +83,7 @@ const run = (rules) => {
     const matches = rules[r]["matches"];
 
     for (let i = 0; i < matches.length; i++) {
-      if (isURLMatched(window.location.toString(), matches[i])) {
+      if (isHostnameMatched(window.location.hostname, matches[i])) {
         log("Found match for " + r + " on " + matches[i]);
         setElementsToRemove(rules[r]["elementsToRemove"]);
         return;
