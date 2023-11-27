1eb3e2c1a65262b1c15588fbc9a450267a4009f2
fire015 <emailfire@gmail.com>
2023-11-27 12:07:07 +0000

    Update publish task and remove legacy ones

--- tasks/publish.js
+++ tasks/publish.js
@@ -1,27 +1,34 @@
-const fs = require("fs");
-const webStore = require("chrome-webstore-upload")({
+import chromeWebstoreUpload from "chrome-webstore-upload";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const BUILD_FILE = __dirname + "/../dist/build.zip";
+
+const webStore = chromeWebstoreUpload({
   extensionId: process.env.EXTENSION_ID,
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   refreshToken: process.env.REFRESH_TOKEN,
 });
 
-const publishFile = async () => {
+const publishFile = () => {
   return new Promise(async (resolve, reject) => {
     try {
       const token = await webStore.fetchToken();
-      const file = __dirname + "/../build.zip";
-      await uploadFile(file, token);
+      await uploadFile(token);
       await publishExtension(token);
-      fs.unlink(file, resolve);
+      resolve();
     } catch (err) {
       reject(err);
     }
   });
 };
 
-const uploadFile = async (file, token) => {
-  const res = await webStore.uploadExisting(fs.createReadStream(file), token);
+const uploadFile = async (token) => {
+  const res = await webStore.uploadExisting(fs.createReadStream(BUILD_FILE), token);
 
   if (!res.uploadState || res.uploadState !== "SUCCESS") {
     throw new Error("Upload failed, response was: " + JSON.stringify(res));
